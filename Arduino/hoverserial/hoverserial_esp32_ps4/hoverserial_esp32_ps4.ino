d811de0f44b37c1bf8c566c3b19d864532412334
Andyzug <zugravu.andrei@gmail.com>
2023-09-09 23:15:52 -0400

    Adding ps4-esp32 example

---
+++ Arduino/hoverserial/hoverserial_esp32_ps4/hoverserial_esp32_ps4.ino
@@ -0,0 +1,173 @@
+// *******************************************************************
+//  ESP-WROOM-32 example code
+//  for   https://github.com/EmanuelFeru/hoverboard-firmware-hack-FOC
+//
+//  Copyright (C) 2019-2020 Emanuel FERU <aerdronix@gmail.com>
+//
+// *******************************************************************
+// INFO:
+// • You need to install the PS4Controller library from this repo https://github.com/aed3/PS4-esp32
+// • Also you need to install SixaxisPairTool-0.3.1 utility to pair your PS4 controller with the ESP32, see line 65 for the MAC address
+// • The data packaging includes a Start Frame, checksum, and re-syncronization capability for reliable communication
+//
+// CONFIGURATION on the hoverboard side in config.h:
+// • Using Serial on Right Sensor cable (short wired cable) - recommended, since the USART3 pins are 5V tolerant.
+//   #define CONTROL_SERIAL_USART3
+//   #define FEEDBACK_SERIAL_USART3
+//   // #define DEBUG_SERIAL_USART3
+// *******************************************************************
+
+// ########################## DEFINES ##########################
+#define HOVER_SERIAL_BAUD   115200      // [-] Baud rate for HoverSerial (used to communicate with the hoverboard)
+#define SERIAL_BAUD         115200      // [-] Baud rate for built-in Serial (used for the Serial Monitor)
+#define START_FRAME         0xABCD     	// [-] Start frme definition for reliable serial communication
+#define TIME_SEND           100         // [ms] Sending time interval
+#define SPEED_MAX           500        // [-] Maximum speed (0 to 1000)
+// #define DEBUG_RX                        // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
+
+#include <PS4Controller.h>    
+#include <HardwareSerial.h>
+HardwareSerial HoverSerial(1); // use UART1
+
+// Global variables
+uint8_t idx = 0;                        // Index for new data pointer
+uint16_t bufStartFrame;                 // Buffer Start Frame
+byte *p;                                // Pointer declaration for the new received data
+byte incomingByte;
+byte incomingBytePrev;
+
+typedef struct{
+   uint16_t start;
+   int16_t  steer;
+   int16_t  speed;
+   uint16_t checksum;
+} SerialCommand;
+SerialCommand Command;
+
+typedef struct{
+   uint16_t start;
+   int16_t  cmd1;
+   int16_t  cmd2;
+   int16_t  speedR_meas;
+   int16_t  speedL_meas;
+   int16_t  batVoltage;
+   int16_t  boardTemp;
+   uint16_t cmdLed;
+   uint16_t checksum;
+} SerialFeedback;
+SerialFeedback Feedback;
+SerialFeedback NewFeedback;
+
+// ########################## SETUP ##########################
+void setup() 
+{
+  Serial.begin(SERIAL_BAUD);
+  PS4.begin("84:c5:a6:ec:47:c5"); // Add this MAC address to your PS4 controller with SixaxisPairTool v0.3.1
+  Serial.println("Hoverboard Serial v1.0");
+
+  HoverSerial.begin(HOVER_SERIAL_BAUD, SERIAL_8N1, 18, 19); // RX, TX
+//  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// ########################## SEND ##########################
+void Send(int16_t uSteer, int16_t uSpeed)
+{
+  // Create command
+  Command.start    = (uint16_t)START_FRAME;
+  Command.steer    = (int16_t)uSteer;
+  Command.speed    = (int16_t)uSpeed;
+  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
+
+  // Write to Serial
+  HoverSerial.write((uint8_t *) &Command, sizeof(Command)); 
+}
+
+// ########################## RECEIVE ##########################
+void Receive()
+{
+    // Check for new data availability in the Serial buffer
+    if (HoverSerial.available()) {
+        incomingByte 	  = HoverSerial.read();                                   // Read the incoming byte
+        bufStartFrame	= ((uint16_t)(incomingByte) << 8) | incomingBytePrev;       // Construct the start frame
+    }
+    else {
+        return;
+    }
+
+  // If DEBUG_RX is defined print all incoming bytes
+  #ifdef DEBUG_RX
+        Serial.print(incomingByte);
+        return;
+    #endif
+
+    // Copy received data
+    if (bufStartFrame == START_FRAME) {	                    // Initialize if new data is detected
+        p       = (byte *)&NewFeedback;
+        *p++    = incomingBytePrev;
+        *p++    = incomingByte;
+        idx     = 2;	
+    } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+        *p++    = incomingByte; 
+        idx++;
+    }	
+    
+    // Check if we reached the end of the package
+    if (idx == sizeof(SerialFeedback)) {
+        uint16_t checksum;
+        checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+                            ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+
+        // Check validity of the new data
+        if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+            // Copy the new data
+            memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+
+            // Print data to built-in Serial
+            Serial.print("1: ");   Serial.print(Feedback.cmd1);
+            Serial.print(" 2: ");  Serial.print(Feedback.cmd2);
+            Serial.print(" 3: ");  Serial.print(Feedback.speedR_meas);
+            Serial.print(" 4: ");  Serial.print(Feedback.speedL_meas);
+            Serial.print(" 5: ");  Serial.print(Feedback.batVoltage);
+            Serial.print(" 6: ");  Serial.print(Feedback.boardTemp);
+            Serial.print(" 7: ");  Serial.println(Feedback.cmdLed);
+        } else {
+          Serial.println("Non-valid data skipped");
+        }
+        idx = 0;    // Reset the index (it prevents to enter in this if condition in the next cycle)
+    }
+
+    // Update previous states
+    incomingBytePrev = incomingByte;
+}
+
+// ########################## LOOP ##########################
+
+unsigned long iTimeSend = 0;
+int speed = 0;
+int steer = 0;
+
+void loop(void)
+{ 
+  unsigned long timeNow = millis();
+  // Check for new received data
+  Receive();
+
+  if (PS4.LStickY() >= 10 || PS4.LStickY()<= -10 ){ // [-10 ... 10] is joystick deadzone
+    speed = map(PS4.LStickY(), -127, 127, SPEED_MAX * -1, SPEED_MAX);
+  } else {
+    speed = 0;
+  }
+
+  if (PS4.RStickX() >= 10 || PS4.RStickX()<= -10 ){
+    steer = map(PS4.RStickX(), -127, 127, SPEED_MAX * -1, SPEED_MAX);
+  } else {
+    steer = 0;
+  }
+  Send(steer, speed);
+  // Serial.print("steer: ");  Serial.print(steer);
+  // Serial.print("  speed: ");  Serial.println(speed);
+  delay(20);
+
+}
+
+// ########################## END ##########################
